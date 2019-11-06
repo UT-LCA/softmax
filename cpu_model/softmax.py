080a37d4c2831aee0d8676452bc493b5692b33bc
Aman Arora <aman.kbm@gmail.com>
2019-11-05 23:34:27 -0600

    Simple model of softmax

--- cpu_model/softmax.py
+++ cpu_model/softmax.py
@@ -1,5 +1,17 @@
 import numpy as np
 
-input = np.random.rand(1024,1).astype('float16')
-
+#Input array of type float16 populated with random values
+input = np.random.rand(8,1).astype('float16')
 print(input)
+
+#Calculate e^x
+exp_array = np.exp(input)
+print(exp_array)
+
+#Calculate sum(e^x)
+sum = np.sum(exp_array)
+print(sum)
+
+#Find probabilities: e^x/sum(e^x)
+probabilities = exp_array / sum
+print(probabilities)
