baa2b0bdbfc699897582ef78757f902817fff631
Aman Arora <aman.kbm@gmail.com>
2019-11-12 10:15:46 -0600

    Updating code for generation of adder tree

--- generator/generate_softmax.py
+++ generator/generate_softmax.py
@@ -4,76 +4,118 @@ import argparse
 import math
 
 class generate_addertree():
-	def __init__(self, num_inputs):
+  def __init__(self, num_inputs):
     self.num_inputs = num_inputs
     if (self.num_inputs==1):
       raise SystemError("num_inputs = 1 is not supported yet for the adder tree")
     if (self.num_inputs % 2 != 0):
       raise SystemError("num_inputs not a multiple of 2 is not supported yet for the adder tree")
-    self.num_stages = math.log(num_inputs,2)
+    self.num_stages = int(math.log(num_inputs,2))
+    self.printit()
   
   def printit(self):
-		print("module mode4_adderTreee(")
-    for iter in self.num_inputs:
-	    print("inp%d, " % iter)
-    print("ex_inp")
-    print("outp")
-
-    for iter in self.num_inputs:
-      print("input  [`DATAWIDTH-1 : 0] inp%d; " % iter)
-    print("input  [`DATAWIDTH-1 : 0] ex_inp;")
-    print("output [`DATAWIDTH-1 : 0] outp;")
-
-		for iter in int(self.num_stages):
-			print("wire   [`DATAWIDTH-1 : 0] add%d_out;" % iter)
-	  print("wire   [`DATAWIDTH-1 : 0] add%d_out;" % int(self.num_stages))
-
-    i = self.num_inputs/2
-    while i<:
-      print("DW_fp_add #(`MANTISSA, `EXPONENT, `IEEE_COMPLIANCE) add0(.a(inp%d),       .b(inp%d),      .z(add%d_out), .rnd(3'b000),    .status());" % stage, stage+1, stage)
+    print("module mode4_adderTreee(")
+    for iter in range(self.num_inputs):
+      print("  inp%d, " % iter)
+    print("  ex_inp,")
+    print("  outp,")
+    print("  clk,")
+    print("  reset")
+    print(");")
     
 
-class generate_maxtree():
-	def __init__(self, num_inputs):
-    self.num_inputs = num_inputs
-    if (self.num_inputs==1):
-      raise SystemError("num_inputs = 1 is not supported yet for the max tree")
-    if (self.num_inputs % 4 != 0):
-      raise SystemError("num_inputs not a multiple of 4 is not supported yet for the max tree")
-    self.num_stages = math.log(num_inputs,2)
-  
-  def printit(self):
-		print("module mode1_max(")
-    for iter in self.num_inputs:
-	    print("inp%d, " % iter)
-    print("ex_inp")
-    print("outp")
-
-    for iter in self.num_inputs:
+    print("input clk;")
+    print("input reset;")
+    for iter in range(self.num_inputs):
       print("input  [`DATAWIDTH-1 : 0] inp%d; " % iter)
     print("input  [`DATAWIDTH-1 : 0] ex_inp;")
     print("output [`DATAWIDTH-1 : 0] outp;")
+    print("")
 
-		for iter in int(self.num_stages):
-			print("wire   [`DATAWIDTH-1 : 0] cmp%d_out;" % iter)
-	  print("wire   [`DATAWIDTH-1 : 0] cmp%d_out;" % int(self.num_stages))
+    num_adders_in_stage0 = self.num_inputs/2
+    input_num = 0
+    output_num = 0
+    for num_adder in range(num_adders_in_stage0):
+      print("wire   [`DATAWIDTH-1 : 0] add%d_out_stage0;" % input_num)
+      print("reg    [`DATAWIDTH-1 : 0] add%d_out_stage0_reg;" % (input_num))
+      print("always @(posedge clk) begin")
+      print("  if (reset) begin")
+      print("    add%d_out_stage0_reg <= 0;" % (input_num))
+      print("  end else begin")
+      print("    add%d_out_stage0_reg <= add%d_out_stage0;" % (input_num, input_num))
+      print("  end")
+      print("end")
+      print("DW_fp_add #(`MANTISSA, `EXPONENT, `IEEE_COMPLIANCE) add0(.a(inp%d),       .b(inp%d),      .z(add%d_out_stage0), .rnd(3'b000),    .status());" % (input_num, input_num+1, output_num))
+      print("")
+      input_num = input_num + 2
+      output_num = output_num + 1
 
-   
+    for stage in range(self.num_stages-1):
+      num_adders_in_current_stage = (1<<(self.num_stages-stage-2))
+      num_adder_cur_stage = 0
+      num_adder_next_stage = 0
+      for num_adder in range(num_adders_in_current_stage):
+          print("wire   [`DATAWIDTH-1 : 0] add%d_out_stage%d;" % (num_adder_next_stage, stage+1))
+          print("reg    [`DATAWIDTH-1 : 0] add%d_out_stage%d_reg;" % (num_adder_next_stage, stage+1))
+          print("always @(posedge clk) begin")
+          print("  if (reset) begin")
+          print("    add%d_out_stage%d_reg <= 0;" % (num_adder_next_stage, stage+1))
+          print("  end else begin")
+          print("    add%d_out_stage%d_reg <= add%d_out_stage%d;" % (num_adder_next_stage, stage+1, num_adder_next_stage, stage+1))
+          print("  end")
+          print("end")
+          print("DW_fp_add #(`MANTISSA, `EXPONENT, `IEEE_COMPLIANCE) add0(.a(add%d_out_stage%d_reg),       .b(add%d_out_stage%d_reg),      .z(add%d_out_stage%d), .rnd(3'b000),    .status());" % (num_adder_cur_stage, stage, num_adder_cur_stage+1, stage, num_adder_next_stage, stage+1))
+          print("")
+          num_adder_next_stage = num_adder_next_stage + 1
+          num_adder_cur_stage = num_adder_cur_stage + 2
+      
+    print("DW_fp_add #(`MANTISSA, `EXPONENT, `IEEE_COMPLIANCE) add0(.a(add0_out_stage%d),       .b(ex_inp),      .z(outp), .rnd(3'b000),    .status());" % (self.num_stages-1))
+    print("")
+    print("endmodule")
+    
+generate_addertree(16)
 
-class generate_softmax():
-	def __init__(self):
-		pass
-	
-	def parse_args(self):
-		parser = argparse.ArgumentParser()
-		parser.add_argument("-p",
-												"--num_inp_pins",
-												action='store',
-												default=4,
-												help='Number of input pins on the softmax block')
-		parser.add_argument("-v",
-												"--num_inp_vals",
-												action='store',
-												default=4,
-												help='Number of input values to be handled by the softmax block')
-
+#class generate_maxtree():
+#  def __init__(self, num_inputs):
+#    self.num_inputs = num_inputs
+#    if (self.num_inputs==1):
+#      raise SystemError("num_inputs = 1 is not supported yet for the max tree")
+#    if (self.num_inputs % 4 != 0):
+#      raise SystemError("num_inputs not a multiple of 4 is not supported yet for the max tree")
+#    self.num_stages = math.log(num_inputs,2)
+#  
+#  def printit(self):
+#    print("module mode1_max(")
+#    for iter in self.num_inputs:
+#      print("inp%d, " % iter)
+#    print("ex_inp")
+#    print("outp")
+#
+#    for iter in self.num_inputs:
+#      print("input  [`DATAWIDTH-1 : 0] inp%d; " % iter)
+#    print("input  [`DATAWIDTH-1 : 0] ex_inp;")
+#    print("output [`DATAWIDTH-1 : 0] outp;")
+#
+#    for iter in int(self.num_stages):
+#      print("wire   [`DATAWIDTH-1 : 0] cmp%d_out;" % iter)
+#    print("wire   [`DATAWIDTH-1 : 0] cmp%d_out;" % int(self.num_stages))
+#
+#   
+#
+#class generate_softmax():
+#  def __init__(self):
+#    pass
+#  
+#  def parse_args(self):
+#    parser = argparse.ArgumentParser()
+#    parser.add_argument("-p",
+#                        "--num_inp_pins",
+#                        action='store',
+#                        default=4,
+#                        help='Number of input pins on the softmax block')
+#    parser.add_argument("-v",
+#                        "--num_inp_vals",
+#                        action='store',
+#                        default=4,
+#                        help='Number of input values to be handled by the softmax block')
+#
