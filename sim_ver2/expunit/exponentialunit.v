d3d744dd48a20bae3bb8f4f88263018cb65a3f59
Pragnesh <pragneshp1998@gmail.com>
2019-11-16 20:35:55 -0600

    changed port names to DesignWare lib port names

--- sim_ver2/expunit/exponentialunit.v
+++ sim_ver2/expunit/exponentialunit.v
@@ -1,20 +1,20 @@
 `timescale 1ns / 1ps
 
-module expunit (fpin, fpout);
+module expunit (a, z, status);
 
 	parameter int_width = 3; // fixed point integer length
 	parameter frac_width = 4; // fixed point fraction length
 		
-	input [15:0] fpin;
-	output [15:0] fpout;
-
+	input [15:0] a;
+	output [15:0] z;
+	output [7:0] status;
 
 	wire [int_width + frac_width - 1: 0] fxout;
 	wire [31:0] LUTout;
 	wire [15:0] Mult_out;
 
-	fptofixed_para fpfx (.fp(fpin), .fx(fxout));
+	fptofixed_para fpfx (.fp(a), .fx(fxout));
 	LUT lut(.addr(fxout[int_width + frac_width - 1 : 0]), .exp(LUTout)); 
-	DW_fp_mult fpmult (.a(fpin), .b(LUTout[31:16]), .rnd(3'b000), .z(Mult_out), .status());
-	DW_fp_add fpsub (.a(Mult_out), .b(LUTout[15:0]), .rnd(3'b000), .z(fpout), .status());
+	DW_fp_mult fpmult (.a(a), .b(LUTout[31:16]), .rnd(3'b000), .z(Mult_out), .status());
+	DW_fp_add fpsub (.a(Mult_out), .b(LUTout[15:0]), .rnd(3'b000), .z(z), .status(status[7:0]));
 endmodule
--- sim_ver2/logunit/logunit.v
+++ sim_ver2/logunit/logunit.v
@@ -1,16 +1,17 @@
 `timescale 1ns / 1ps
-module logunit (fpin, fpout);
+module logunit (a, z, status);
 
 	
-	input [15:0] fpin;
-	output [15:0] fpout;
-
+	input [15:0] a;
+	output [15:0] z;
+	output [7:0] status;
 
 	wire [15: 0] fxout1;
 	wire [15: 0] fxout2;
 
 
-	LUT1 lut1 (.addr(fpin[14:10]),.log(fxout1)); 
-	LUT2 lut2 (.addr(fpin[9:0]),.log(fxout2));  
-	DW_fp_addsub add(.a(fxout1), .b(fxout2), .rnd(3'b0), .op(1'b0), .z(fpout), .status());
+	LUT1 lut1 (.addr(a[14:10]),.log(fxout1)); 
+	LUT2 lut2 (.addr(a[9:0]),.log(fxout2));  
+	DW_fp_addsub add(.a(fxout1), .b(fxout2), .rnd(3'b0), .op(1'b0), .z(z), .status(status[7:0]));
+	
 endmodule
