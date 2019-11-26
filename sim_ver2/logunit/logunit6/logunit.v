c6bd3106ca53351a0d485ed5c1517d4e5c09344f
Pragnesh <pragneshp1998@gmail.com>
2019-11-25 23:55:59 -0600

    adding Logunit with LUT6

---
+++ sim_ver2/logunit/logunit6/logunit.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+module logunit (fpin, fpout);
+
+	
+	input [15:0] fpin;
+	output [15:0] fpout;
+
+
+	wire [15: 0] fxout1;
+	wire [15: 0] fxout2;
+
+
+	LUT1 lut1 (.addr(fpin[14:10]),.log(fxout1)); 
+	LUT2 lut2 (.addr(fpin[9:4]),.log(fxout2));  
+	DW_fp_addsub add(.a(fxout1), .b(fxout2), .rnd(3'b0), .op(1'b0), .z(fpout), .status());
+endmodule
