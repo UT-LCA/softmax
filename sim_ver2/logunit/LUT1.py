681168605074ac88698e94a034e3869fbfc052d3
Pragnesh <pragneshp1998@gmail.com>
2019-11-27 17:13:54 -0600

    created separate folders for all versions of log and exp units and added the designfiles along with python files to generate LUT

---
+++ sim_ver2/logunit/LUT1.py
@@ -0,0 +1,24 @@
+#use this to generate LUT-EXP for float16
+import numpy as np
+import math
+
+a = np.float64(1)
+print("module LUT1(addr, log);")
+print("    input [4:0] addr;")
+print("    output reg [15:0] log;")
+print("")
+print("    always @(addr) begin")
+print("        case (addr)")
+print("			5'b0"),
+print("		: log = 16'b1111110000000000;")
+for i in range (30):
+	temp = np.log(2)*(a-15)
+	num = bin(np.float16(temp).view('H'))[2:].zfill(16)
+	print("			5'b{0:b}".format(i+1).zfill(7)),
+	print("		: log = 16'b{0};".format(num))
+	a += 1
+print("			5'b11111"),
+print("		: log = 16'b0111110000000000;")
+print("        endcase")
+print("    end")
+print("endmodule")
